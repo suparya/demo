7c7b5fa315033a3e7b2fea8a1ff57c3ece9bbc28
suparya <suparya.das@hirepro.in>
2019-09-19 11:36:19 +0530

    first commit with calculator App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  num1: number;
+  num2: number;
+  result: number;
+
+  add() {
+
+    this.result = this.num1 + this.num2;
+
+  }
+
+  substract() {
+
+    this.result = this.num1 - this.num2;
+
+  }
+
+  multiply() {
+
+    this.result = this.num1 * this.num2;
+
+  }
+
+  divide() {
+
+    this.result = this.num1 % this.num2;
+
+  }
 }
